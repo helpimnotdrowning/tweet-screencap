e6f5dc5b89ff5f7970784770dcb4b75cdd6c98e1
helpimnotdrowning <35247379+helpimnotdrowning@users.noreply.github.com>
2021-08-22 20:45:29 -0500

    Last update before rewrite
    This used "hdutil.py", which does not exist and will not be uploaded. This is only being pushewd for history's sake. No idea what was added here, but it won't matter in a bit.

--- tweet_screencap.py
+++ tweet_screencap.py
@@ -1,4 +1,4 @@
-# if you have problems, make an issue here --> https://github.com/helpimnotdrowning/random-py3/issues/new <-- and ill try to help
+# if you have problems, make an issue here --> https://github.com/helpimnotdrowning/tweet-screencap/issues/new <-- and ill try to help
 
 # REQUIRES FFMPEG + FFPROBE IN PATH ( install instructions here --> https://video.stackexchange.com/a/20496 )
 # IF YOU DONT HAVE FFMPEG INSTALLED IT WILL CRASH
@@ -11,16 +11,23 @@ import sys
 import tweepy # twitter api 
 import logging
 import schedule # schedule send tweets
-try: # check if get_frame is downloaded
-    import get_frame # get frame
-except ModuleNotFoundError:
-    raise ModuleNotFoundError('get_frame.py not in current directory. Download from https://github.com/helpimnotdrowning/random-py3/blob/master/get_frame.py')
 import subprocess # use ffprobe to check video length
 
-from time import sleep, strftime, gmtime # make cpu not explode, time formatting
+from time import sleep # make cpu not explode, time formatting
 from random import choices, random # for choosing how much time to proceed each frame
 from socket import timeout # catch some errors hopefully
 
+try:
+    import get_frame
+except ModuleNotFoundError:
+    raise ModuleNotFoundError('get_frame.py not in current directory. Download from https://github.com/helpimnotdrowning/random-py3/blob/master/get_frame.py')
+    
+try:
+    from hdutil import time_to_seconds, seconds_to_time
+    import hdutil
+except ModuleNotFoundError:
+    raise ModuleNotFoundError('hdutil.py not in current directory. Download from https://github.com/helpimnotdrowning/random-py3/blob/master/hdutil.py')
+    
 # setup tweepy auth
 # get yer own keys
 consumer_key = "0"
@@ -32,30 +39,47 @@ access_token_secret = "0"
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
-api = tweepy.API(auth, timeout=120)
+api = tweepy.API(auth)
 
 sec = 1
 video_index = 0
 season_index = 0
 
-s1 = [
+
+class Season:
+    def __init__(self, name:str, videos: list):
+        self.name = name
+        self.videos = videos
+        
+       
+class Series:
+    def __init__(self, name:str, seasons: list[Season]):
+        self.name = name
+        self.seasons = seasons
+        
+s1_epsodes = [
     'put/path_to/1st_video.here',
     'and_the/second_one.here',
     'and_just_keep/doing.that',
     'and_dont/put/a_comma_after_the/last.item'
 ]
-s2 = [
+
+s2_episodes = [
     'you_can/also_make_another/set/season_after_the/last.one',
     'you_can_name_the_sets/whatever/you.like',
 ]
 
-movie = [
+movie_episodes = [
     'just_remember_to/put_them_in_the_correct.order',
     r'and\for_windows_paths\with_the_other_slashes_you\need_to_have_an_r_before\the.quote'
 ]
+ 
+s1    = Season('Series Name! Season 1 (Series!)', s1_epsodes)
+s2    = Season('Series Name! Season 2 (Series!!)', s2_episodes)
+movie = Season('Series Name!: The Movie', movie_episodes)
 
 # if they arent in correct order, it will mess up
-series = [ s1, s2, movie ]
+series = Series('Series Name!', [ s1, s2, movie ])
 
 # setup logging
 logger = logging.getLogger("main")
@@ -67,41 +91,9 @@ ch.setLevel(logging.DEBUG)
 ch.setFormatter(logging.Formatter('[%(asctime)s.%(msecs)03d] [%(name)s/%(levelname)s]: %(message)s', '%H:%M:%S'))
 logger.addHandler(ch)
 
+dont_save = False
 
-# helper functions to make code prettier
-def write_file(file, content):
-    with open(file, 'w+') as file_:
-        file_.write(str(content))
-        
-        
-def read_file(file):
-    with open(file, 'r') as file_:
-        return file_.read()
-        
-        
-def read_image(file):
-    return cv2.imread(file)
-    
-    
-def delete_file(file):
-    try:
-        os.remove(file)
-    except FileNotFoundError:
-        pass
-        
-        
-def time_to_seconds(hours=0, minutes=0, seconds=0, ms = 0):
-    hour_in_minutes = hours * 60
-    minutes += hour_in_minutes
-    seconds += (minutes * 60) + (ms * .001)
-    return float(seconds)
-    
-    
-def seconds_to_time(seconds):
-    seconds = float(seconds)
-    return strftime("%H:%M:%S", gmtime(seconds)) + str(round(seconds % 1, 3))[1:]
-    
-    
+
 def read_state():
     global sec
     global season_index
@@ -109,16 +101,16 @@ def read_state():
     
     # if its the first time running the bot (at least in current directory), make a "pick up" file,
     # so in case the bot crashes, pc crashes, blackout, the bot can pick up where it left off
-    if not os.path.exists("pick_up_time.txt"):
-        write_file("pick_up_time.txt", "1") # start at beginning of video
+    if not os.path.exists("./pick_up_time.txt"):
+        hdutil.write_file("./pick_up_time.txt", "1") # start at beginning of video
         
-    if not os.path.exists('pick_up_video.txt'):
-        write_file("pick_up_video.txt", '0') # start at first video
+    if not os.path.exists('./pick_up_video.txt'):
+        hdutil.write_file("./pick_up_video.txt", '0') # start at first video
         
-    if not os.path.exists('pick_up_season.txt'):
-        write_file("pick_up_season.txt", "0") # start at first season
+    if not os.path.exists('./pick_up_season.txt'):
+        hdutil.write_file("./pick_up_season.txt", "0") # start at first season
         
-    _sec = read_file("pick_up_time.txt")
+    _sec = hdutil.read_file("./pick_up_time.txt")
     
     # check if pick_up_time.txt is a tuple, if it is separate the 4 numbers expected into a list
     if str(_sec).startswith('('):
@@ -130,13 +122,14 @@ def read_state():
             
         # set sec to that
         sec = float(time_to_seconds(time_split[0], time_split[1], time_split[2], time_split[3]))
-    
+        
     # else, if its just a normal number like what this script saves, dont do funny stuff with it
     else:
         sec = float(_sec)
         
-    season_index = int(read_file("pick_up_season.txt"))
-    video_index = int(read_file("pick_up_video.txt"))
+    season_index = int(hdutil.read_file("./pick_up_season.txt"))
+    
+    video_index = int(hdutil.read_file("./pick_up_video.txt"))
     
     logger.debug('Picking up at season %s, video %s at %s (%s)', season_index + 1, video_index + 1, seconds_to_time(sec), sec)
     
@@ -147,25 +140,26 @@ def save_state():
     if dont_save == True:
         logger.debug('State not saved.')
     else:
-        write_file("pick_up_time.txt", sec)
-        write_file("pick_up_video.txt", video_index)
-        write_file("pick_up_season.txt", season_index)
-    
-    
+        hdutil.write_file("./pick_up_time.txt", sec)
+        hdutil.write_file("./pick_up_video.txt", video_index)
+        hdutil.write_file("./pick_up_season.txt", season_index)
+        
+        
 def reset_state():
     logger.warning('Clearing state...')
-    delete_file("pick_up_time.txt")
-    delete_file("pick_up_video.txt")
-    delete_file("pick_up_season.txt")
-
-
+    
+    delete_file("./pick_up_time.txt")
+    delete_file("./pick_up_video.txt")
+    delete_file("./pick_up_season.txt")
+    exit()
+    
+    
 # upload image to twitter, keep retrying untill success
-#@tenacity.retry
 def upload_image_to_twitter(path):
     logger.debug('Uploading image to Twitter...')
     
     if api == None:
-        cv2.imwrite('upload.png', read_image(path))
+        cv2.imwrite('upload.png', hdutil.read_image(path))
         return 0
     else:
         logger.warning('ACTUALLY UPLOADING FOR REAL THIS TIME!!!!')
@@ -173,9 +167,8 @@ def upload_image_to_twitter(path):
             return api.media_upload(filename=path).media_id_string
         except timeout:
             raise timeout('Timed out.')
-        
+            
 # send tweet, keep retrying untill success
-#@tenacity.retry
 def send_tweet_with_media(media_id):
     logger.debug('Sending Tweet...')
     
@@ -189,7 +182,6 @@ def send_tweet_with_media(media_id):
     
     
 # send tweet, keep retrying untill success
-#@tenacity.retry
 def send_tweet_with_text(status_text):
     logger.debug('Sending Tweet...')
     
@@ -202,6 +194,16 @@ def send_tweet_with_text(status_text):
             raise timeout('Timed out.')
         #except tweepy.error.TweepError as e:
             #if e.api_code == '
+            
+            
+def update_bio(bio_text):
+    if api == None:
+        logger.info('Bio would have been sent here.')
+    else:
+        try:
+            api.update_profile(description="""TODO: add description """)
+        except timeout:
+            raise timeout('Timed out.')
             
             
 def randrange_OLD():
@@ -218,7 +220,7 @@ def randrange():
     #wgt = [12,28,28,28] # out of 100
     rand_choice = choices(seq, weights = wgt)
     # choices returns a list
-    return rand_choice[0] + round(random(), 2) # tack on some milliseconds so it cant only send frames on the 0
+    return rand_choice[0] + round(random(), 3) # tack on some milliseconds so it cant only send frames on the 0
     
     
 # https://stackoverflow.com/a/3844467
@@ -237,18 +239,19 @@ def get_video():
     
     # try to set season, if more seasons than exist (series over), exit
     try:
-        season = series[season_index]
+        season = series.seasons[season_index]
     except IndexError:
         exit()
-    
+        
     # try to set video, if more videos than exist (season over), go to next season and reset video to 0
     try:
-        video = season[video_index]
+        video = season.videos[video_index]
+        print(video, video_index)
     except IndexError:
         season_index += 1
         video_index = 0
         video = get_video()
-    
+        
     return video
     
     
@@ -259,8 +262,8 @@ def next_video_please():
     video_index += 1
     sec = 0
     logger.info("Episode over, switching to next.")
-    
-    
+
+
 # main function
 def tweet_frame():
     global sec
@@ -270,6 +273,7 @@ def tweet_frame():
     
     video = get_video()
     vid_length = get_length(video)
+    logger.critical(type(sec))
     
     if sec <= vid_length:
     
@@ -282,6 +286,7 @@ def tweet_frame():
         logger.debug('sec of %s is less than or equal to length %s', str(sec), str(vid_length))
         
         if success:
+            #print(frame)
             cv2.imwrite("tmp.png", frame)
             
             # upload frame to twitter, then get the media ID from that
@@ -291,10 +296,10 @@ def tweet_frame():
             send_tweet_with_media(image_id)
             logger.info('Tweet sent with %s', time_string)
             
-            delete_file("tmp.png")
-            
             # go forward for next frame
             sec += randrange()
+                
+            hdutil.delete_file("tmp.png")
             
             time_string = f'frame {seconds_to_time(sec)} ({str(sec)}) from season {str(season_index + 1)}, video {str(video_index + 1)}'
             
@@ -312,17 +317,40 @@ def tweet_frame():
                 raise RuntimeError("Failed to extract frame at {sec}, Dont know how to deal with this.")
                 
     else:
-        video_index += 1
-        sec = 0
-        logger.warning("Episode over, switching.")
-        #exit()
+        next_video_please()
     sleep(1)
-    
-    
+        
+        
 if __name__ == '__main__':
     print(sys.argv)
     
     read_state()
+    
+    if '-set_time' in sys.argv:
+        #api = None
+        #dont_save = True
+        #logger.warning('Starting in test mode.')
+        
+        _time_flag = sys.argv[sys.argv.index('-set_time') + 1]
+        
+        if str(_time_flag).startswith('('):
+            _time_split = str(_time_flag).replace('(','').replace(')','').split(',')
+            
+            # convert them all to numbers
+            for i in range(len(_time_split)):
+                _time_split[i] = int(_time_split[i])
+                
+            # set sec to that
+            sec = float(time_to_seconds(_time_split[0], _time_split[1], _time_split[2], _time_split[3]))
+            print(sec)
+            
+        # else, if its just a normal number like what this script saves, dont do funny stuff with it
+        else:
+            sec = float(_time_flag)
+        
+        save_state()
+        print('New time saved: {} ({})'.format(seconds_to_time(sec),sec))
+        #sec = float(sys.argv[sys.argv.index('-set_time') + 1])
     
     if '-test' in sys.argv:
         api = None
@@ -332,7 +360,7 @@ if __name__ == '__main__':
     if '-reset' in sys.argv:
         if '-test' in sys.argv:
             logger.warning('State not cleared. Exiting anyway.')
-            exit()
+            reset_state()
             
         elif '-y' in sys.argv:
             reset_state()
@@ -348,7 +376,12 @@ if __name__ == '__main__':
             
     if '-tweet_now' in sys.argv:
         if '-test' in sys.argv:
-            tweet_frame()
+            times_tweet = sys.argv[sys.argv.index('-tweet_now') + 1]
+            try: times_tweet = int(times_tweet)
+            except: times_tweet = 1
+            
+            for i in range(times_tweet):
+                tweet_frame()
             
         else:
             ask_tweet = input('Send tweet now? (y/n) : ')
